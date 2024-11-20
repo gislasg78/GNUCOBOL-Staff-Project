53f0ff26e663dd616e373bb8d1cf4798af290195
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2024-11-20 08:04:14 -0600

    Text Windows Frames Program.
    This program, in a very preliminary way, generates frames to simulate text windows on the screen under GNUCOBOL and lays the minimum foundation for creating custom frames in text mode for future software developments in the language.

--- filex.cob
+++ filex.cob
@@ -1,118 +1,116 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. filex.
+       PROGRAM-ID. screx.
 
-       ENVIRONMENT DIVISION.
-       CONFIGURATION SECTION.
-       SPECIAL-NAMES.
-            SYMBOLIC asterisk 43
-            CLASS alphabetic-and-numeric IS SPACE,
-					'0' THRU '9',
-					'A' THRU 'Z',
-					'a' THRU 'z'.
-
-       INPUT-OUTPUT SECTION.
-       FILE-CONTROL.
-            SELECT OPTIONAL myfilex ASSIGN TO ws-f-name-myfilex
-                   ORGANIZATION IS LINE SEQUENTIAL
-                   FILE  STATUS IS fs-myfilex.
-            
        DATA DIVISION.
-       FILE SECTION.
-       FD  myfilex
-           DATA RECORD     IS f-rec-myfilex
-           RECORD CONTAINS 20 CHARACTERS
-
-           LINAGE IS 25 LINES
-             WITH FOOTING AT 21
-            LINES AT TOP 2
-            LINES AT BOTTOM 3.
-
-       01  f-rec-myfilex            PIC X(20)  VALUE SPACES.
-           88  sw-f-rec-myfilex-empty          VALUE SPACES.
-       
        WORKING-STORAGE SECTION.
-       77  fs-myfilex               PIC 9(02)  VALUE ZEROES.
-       77  ws-linage-cnt-ed         PIC +9(08) VALUE ZEROES.
+       01  ws-cte-01                    PIC 9(01)  VALUE 01.
 
-       01  ws-cnt-ins-rows          PIC S9(03) VALUE ZEROES
-                                    SIGN LEADING SEPARATE.
-       01  ws-cte-01                PIC 9(01)  VALUE 01.
+       01  ws-screen-coords.
+           03  ws-screen-bounds.
+               05  ws-horizontal-bounds.
+                   07  ws-bottom-row    PIC 9(02)  VALUE 01.
+                   07  ws-top-row       PIC 9(02)  VALUE 24.
+               05  ws-vertical-bounds.
+                   07  ws-left-col      PIC 9(02)  VALUE 01.
+                   07  ws-right-col     PIC 9(02)  VALUE 80.
 
-       01  ws-answer	            VALUE SPACE PIC A(01).
-           88  sw-exit-answer       VALUES ARE 'N', 'n'.
+           03  ws-screen-initiators.
+               05  ws-horizontal-initiators.
+                   07  ws-start-row     PIC 9(02)  VALUE ZEROES.
+                   07  ws-finish-row    PIC 9(02)  VALUE ZEROES.
+               05  ws-vertical-initiators.
+                   07  ws-start-col     PIC 9(02)  VALUE ZEROES.
+                   07  ws-finish-col    PIC 9(02)  VALUE ZEROES.
 
-       01  ws-f-name-myfilex        PIC X(12)  VALUE SPACES.
-       01  ws-f-rec-myfilex         PIC X(80)  VALUE SPACES.
-       
+           03  ws-screen-printing-chars.
+               05  ws-horizontal-chars.
+                   07  ws-bottom-char   PIC X(01)  VALUE X'2D'.
+                   07  ws-top-char      PIC X(01)  VALUE X'3D'.
+               05  ws-vertical-chars.
+                   07  ws-left-char     PIC X(01)  VALUE X'21'.
+                   07  ws-right-char    PIC X(01)  VALUE X'7C'.
+               05  ws-corner-char       PIC X(01)  VALUE X'2B'.
+
+           03  ws-screen-displacement-vars.
+               05  ws-col               PIC 9(02)  VALUE ZEROES.
+               05  ws-row               PIC 9(02)  VALUE ZEROES.
+               05  ws-char              PIC X(01)  VALUE SPACE.
+
        PROCEDURE DIVISION.
-       DECLARATIVES.
-       FL-ERROR-HANDLER SECTION.
-           USE AFTER ERROR PROCEDURE ON myfilex.
-       STATUS-CHECK.
-           DISPLAY "File name  : " ws-f-name-myfilex
-           DISPLAY "Error code : " fs-myfilex.
-       END DECLARATIVES.
-
        MAIN-PARAGRAPH.
-           DISPLAY SPACE UPON CRT
-           DISPLAY "Sequential Organizing File Creator and Generator."
-                AT LINE 1 COLUMN 15 UPON CRT WITH BLANK SCREEN
-           DISPLAY "File name to open: " AT 0203 UPON CRT
-              WITH BLANK LINE
-           ACCEPT ws-f-name-myfilex AT LINE 02 COL 22 FROM CRT
-              WITH PROMPT
+           DISPLAY SPACE WITH BLANK SCREEN
 
-           OPEN EXTEND myfilex
 
-           PERFORM WITH TEST AFTER VARYING ws-cnt-ins-rows
-              FROM ws-cte-01 BY ws-cte-01 UNTIL sw-exit-answer
-                   DISPLAY "Enter a text line to be recorded: "
-                        AT LINE NUMBER 04 COLUMN NUMBER 03 UPON CRT
-                      WITH BLANK LINE
-                    ACCEPT ws-f-rec-myfilex
-                        AT LINE NUMBER 04 COLUMN NUMBER 37 FROM CRT
-                      WITH PROMPT UPDATE
+           MOVE ws-left-char   TO ws-char
+           MOVE ws-left-col    TO ws-start-col
+           MOVE ws-bottom-row  TO ws-start-row
+           MOVE ws-top-row     TO ws-finish-row
 
-                    IF ws-f-rec-myfilex IS alphabetic-and-numeric
-                       CONTINUE
-                    ELSE
-                       DISPLAY "Input line contains other characters: "
-                            AT LINE 06 COLUMN 03 UPON CRT
-                               ws-f-rec-myfilex
-                            AT LINE 06 COLUMN 41 UPON CRT
-                    END-IF
+           PERFORM 000000-begin-build-vertical-lines
+              THRU 000000-end-build-vertical-lines
+           VARYING ws-row FROM ws-start-row BY ws-cte-01
+             UNTIL ws-row IS GREATER THAN ws-finish-row
 
-                    WRITE f-rec-myfilex  FROM ws-f-rec-myfilex
-                       AT END-OF-PAGE
-                          MOVE LINAGE-COUNTER TO ws-linage-cnt-ed
-                          DISPLAY "Line break has occurred on line: "
-                               AT LINE 08 COLUMN 03 UPON CRT
-                                  ws-linage-cnt-ed
-                               AT LINE 08 COL 36 UPON CRT
-                             WITH REVERSE-VIDEO
+           MOVE ws-right-char  TO ws-char
+           MOVE ws-right-col   TO ws-start-col
+           MOVE ws-bottom-row  TO ws-start-row
+           MOVE ws-top-row     TO ws-finish-row
 
-                          SET sw-f-rec-myfilex-empty TO TRUE
-                          WRITE f-rec-myfilex AFTER ADVANCING PAGE
-                          DISPLAY asterisk AT LINE 24 COL 40 UPON CRT
+           PERFORM 000000-begin-build-vertical-lines
+              THRU 000000-end-build-vertical-lines
+           VARYING ws-row FROM ws-start-row BY ws-cte-01
+             UNTIL ws-row IS GREATER THAN ws-finish-row
 
-                     NOT AT EOP
-                          MOVE LINAGE-COUNTER TO ws-linage-cnt-ed
-                          DISPLAY "Inserted line: " AT 1003 UPON CRT
-                          DISPLAY ws-linage-cnt-ed  AT 1018 UPON CRT
-                             WITH REVERSE-VIDEO
-                    END-WRITE
 
-                    DISPLAY "Want to capture more lines of text (Y/N)? "
-                         AT LINE 12 COL 03 UPON CRT
-                     ACCEPT ws-answer AT LINE 12 COLUMN 45 FROM CRT
-                       WITH PROMPT UPDATE
-           END-PERFORM
+           MOVE ws-top-char    TO ws-char
+           MOVE ws-top-row     TO ws-start-row
+           MOVE ws-left-col    TO ws-start-col
+           MOVE ws-right-col   TO ws-finish-col
 
-           DISPLAY "Rows inserted into output file: " AT 1403 UPON CRT
-                   ws-cnt-ins-rows AT 1435 UPON CRT
+           PERFORM 000000-begin-build-horizontal-lines
+              THRU 000000-end-build-horizontal-line
+           VARYING ws-col FROM ws-start-col BY ws-cte-01
+             UNTIL ws-col IS GREATER THAN ws-finish-col
 
-           CLOSE myfilex
+           MOVE ws-bottom-char TO ws-char
+           MOVE ws-bottom-row  TO ws-start-row
+           MOVE ws-left-col    TO ws-start-col
+           MOVE ws-right-col   TO ws-finish-col
+
+           PERFORM 000000-begin-build-horizontal-lines
+              THRU 000000-end-build-horizontal-line
+           VARYING ws-col FROM ws-start-col BY ws-cte-01
+             UNTIL ws-col IS GREATER THAN ws-finish-col
+
+
+           MOVE ws-corner-char TO ws-char
+           MOVE ws-top-row     TO ws-start-row
+           MOVE ws-bottom-row  TO ws-finish-row
+           MOVE ws-left-col    TO ws-start-col
+           MOVE ws-right-col   TO ws-finish-col
+
+           PERFORM 000000-begin-build-corners
+              THRU 000000-end-build-corners
 
            STOP RUN.
 
-       END PROGRAM filex.
+       000000-begin-build-vertical-lines.
+           DISPLAY ws-char AT LINE ws-row        COLUMN ws-start-col.
+       000000-end-build-vertical-lines.
+           EXIT.
+
+       000000-begin-build-horizontal-lines.
+           DISPLAY ws-char AT LINE ws-start-row  COLUMN ws-col.
+       000000-end-build-horizontal-line.
+           EXIT.
+
+       000000-begin-build-corners.
+           DISPLAY ws-char AT LINE ws-start-row  COLUMN ws-start-col
+           DISPLAY ws-char AT LINE ws-start-row  COLUMN ws-finish-col
+
+           DISPLAY ws-char AT LINE ws-finish-row COLUMN ws-start-col
+           DISPLAY ws-char AT LINE ws-finish-row COLUMN ws-finish-col.
+       000000-end-build-corners.
+           EXIT.
+
+       END PROGRAM screx.
