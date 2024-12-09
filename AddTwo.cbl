aa4de4b7a73bb9e25bce492deffd54fda359118a
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2024-12-09 12:20:28 -0600

    Add Two Numbers Program
    This program only adds two numbers but prevents values ​​from being lost during data entry due to being previously compressed and the receiving field is enlarged enough to accommodate the sum.

--- AddTwo.cbl
+++ AddTwo.cbl
@@ -4,33 +4,42 @@
        DATA DIVISION.
        WORKING-STORAGE SECTION.
        01  ws-environment-variables.
-           03  ws-num1    PIC S9(06)V9(04)    VALUE ZEROES.
-           03  ws-num2    PIC S9(06)V9(04)    VALUE ZEROES.
-           03  ws-result  PIC +Z,ZZZ,ZZZ.ZZZZ VALUE ZEROES.
+           03  ws-amount1  PIC S9(09)V9(04)       COMP   VALUE ZEROES.
+           03  ws-amount2  PIC S9(09)V9(04)       COMP-3 VALUE ZEROES.
+           03  ws-result   PIC $---,---,---,--9.9999     VALUE ZEROES.
 
        PROCEDURE DIVISION.
        MAIN-PARAGRAPH.
            DISPLAY "Program that adds two numbers."
            DISPLAY "Enter the first  number: "
               WITH NO ADVANCING
-            ACCEPT ws-num1
+            ACCEPT ws-amount1
 
            DISPLAY "Enter the second number: "
               WITH NO ADVANCING
-            ACCEPT ws-num2
+            ACCEPT ws-amount2
 
-           ADD ws-num1 TO ws-num2 GIVING ws-result
+           ADD ws-amount1 TO ws-amount2 GIVING ws-result
             ON SIZE ERROR
-               DISPLAY "The calculation is too large "
-                       "to be stored in the receiving"
-                       "variable."
+               DISPLAY SPACE
+               DISPLAY "The calculation is too large to be "
+                       "stored in the receiving variable."
            END-ADD
 
            DISPLAY SPACE
-           DISPLAY "The result of adding the first "
-           "number: [" ws-num1 "] plus the "
-           "second number: [" ws-num2 "] is: "
-           "[" ws-result "]."
+           DISPLAY "Information on added numbers."
+           DISPLAY "First Number.  Type COMP."
+           DISPLAY "Length: [" LENGTH OF ws-amount1 "]."
+           DISPLAY "Value : [" ws-amount1 "]."
+
+           DISPLAY SPACE
+           DISPLAY "Second Number. Type COMP-3."
+           DISPLAY "Length: [" LENGTH OF ws-amount2 "]."
+           DISPLAY "Value : [" ws-amount2 "]."
+
+           DISPLAY SPACE
+           DISPLAY "The result of their sum is: ["
+                   ws-result "]."
 
            STOP RUN.
 
