39fd03ff33d54f327d8b2a71550a005f0754be36
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2024-12-12 10:33:56 -0600

    Add Two Numbers Program
    This program just adds two numbers, but checks that the receiving field size is large enough to accommodate the result, otherwise various error messages are thrown. It is ideal for testing error-based size conditions.

--- AddTwo.cbl
+++ AddTwo.cbl
@@ -6,25 +6,42 @@
        01  ws-environment-variables.
            03  ws-amount1  PIC S9(09)V9(04)       COMP   VALUE ZEROES.
            03  ws-amount2  PIC S9(09)V9(04)       COMP-3 VALUE ZEROES.
-           03  ws-result   PIC $---,---,---,--9.9999     VALUE ZEROES.
+           03  ws-result   PIC $-,---,---,--9.9999      VALUE ZEROES.
 
        PROCEDURE DIVISION.
        MAIN-PARAGRAPH.
            DISPLAY "Program that adds two numbers."
-           DISPLAY "Enter the first  number: "
-              WITH NO ADVANCING
+           DISPLAY "Enter the first  number: " WITH NO ADVANCING
             ACCEPT ws-amount1
 
-           DISPLAY "Enter the second number: "
-              WITH NO ADVANCING
+           DISPLAY "Enter the second number: " WITH NO ADVANCING
             ACCEPT ws-amount2
 
            ADD ws-amount1 TO ws-amount2 GIVING ws-result
-            ON SIZE ERROR
-               DISPLAY SPACE
-               DISPLAY "The calculation is too large to be "
-                       "stored in the receiving variable."
+               ON SIZE ERROR
+                  PERFORM DisplayErrorMessages
+
+              NOT ON SIZE ERROR
+                  PERFORM ProvideResultsInformation
+
            END-ADD
+
+           STOP RUN.
+
+       DisplayErrorMessages.
+           DISPLAY SPACE
+           DISPLAY "The calculation could not be made."
+
+           DISPLAY SPACE
+           DISPLAY "The calculation is too large to be stored in the "
+                   "receiving variable."
+           DISPLAY "The result size exceeds its definition size."
+           DISPLAY "The original result value is respected: "
+                   "[" ws-result "].".
+
+       ProvideResultsInformation.
+           DISPLAY SPACE
+           DISPLAY "The calculation was satisfactory."
 
            DISPLAY SPACE
            DISPLAY "Information on added numbers."
@@ -38,9 +55,6 @@
            DISPLAY "Value : [" ws-amount2 "]."
 
            DISPLAY SPACE
-           DISPLAY "The result of their sum is: ["
-                   ws-result "]."
-
-           STOP RUN.
+           DISPLAY "The result of their sum is: [" ws-result "].".
 
        END PROGRAM AddTwo.
