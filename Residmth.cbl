b598e0b8bc249e2b8d0cade263c6b98bacc96ac5
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-01-11 13:17:40 -0600

    Calc Position Address Record Program
    A program that uses the remainder method to convert keys into relative position addresses, that is, it uses a hashing method to arrange keys into calculated registration numbers.

---
+++ Residmth.cbl
@@ -0,0 +1,57 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Residmth.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       78  cte-100                          VALUE 100.
+
+       01  ws-code-data          PIC 9(06)  VALUE ZEROES.
+       01  ws-quottient          PIC 9(06)  VALUE ZEROES.
+
+       01  ws-key-sat-perc       PIC 9(02)  VALUE ZEROES.
+       01  ws-key-max-sat        PIC 9(06)  VALUE ZEROES.
+
+       01  ws-fac-pos-addr       PIC 9(06)  VALUE ZEROES.
+       01  ws-rel-pos-addr       PIC 9(06)  VALUE ZEROES.
+
+       PROCEDURE DIVISION.
+       MAIN-PARAGRAPH.
+           DISPLAY "Prog that converts a key into a relative position"
+           DISPLAY "Maximum number of records : " WITH NO ADVANCING
+            ACCEPT ws-key-max-sat
+           DISPLAY "Key saturation percentage : " WITH NO ADVANCING
+            ACCEPT ws-key-sat-perc
+           DISPLAY "Enter a six-digit code    : " WITH NO ADVANCING
+            ACCEPT ws-code-data
+
+           DISPLAY SPACE
+           DISPLAY "Calculation information."
+           DISPLAY "Maximum number of records : [" ws-key-max-sat "]."
+           DISPLAY "Key saturation percentage : [" ws-key-sat-perc "]."
+
+           COMPUTE ws-fac-pos-addr = ws-key-max-sat /
+                   (ws-key-sat-perc / cte-100)
+                ON SIZE ERROR
+                   DISPLAY "Error in calculation. "
+                           "The result is invalid."
+           END-COMPUTE
+
+           DISPLAY SPACE
+           DISPLAY "Calculation of the fit factor."
+           DISPLAY "Estimated file density    : [" ws-fac-pos-addr "]."
+
+           DIVIDE ws-fac-pos-addr INTO ws-code-data GIVING ws-quottient
+           REMAINDER ws-rel-pos-addr
+               ON SIZE ERROR
+                  DISPLAY "Error in calculation. "
+                          "The result is invalid."
+           END-DIVIDE
+
+           DISPLAY SPACE
+           DISPLAY "Obtaining the registration number."
+           DISPLAY "Code to be converted      : [" ws-code-data "]."
+           DISPLAY "Relative position address : [" ws-rel-pos-addr "]."
+
+           STOP RUN.
+
+       END PROGRAM Residmth.
