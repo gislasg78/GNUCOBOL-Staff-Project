ab847d57eeb2739349d6046e67cb70094ded2030
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2025-02-17 20:49:22 -0600

    Sales Report Display Program
    The purpose of this program is to display a quarterly sales report for a series of sellers, and returns the annualized sum for each one, as well as totals per quarter, global total, count of records read and the average of global sales.

--- CarSlsRp.cbl
+++ CarSlsRp.cbl
@@ -4,214 +4,218 @@
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-           SELECT OPTIONAL CARSALESFILE ASSIGN TO WS-NAME-CARSALESFILE
+           SELECT OPTIONAL carsalesfile ASSIGN TO ws-name-carsalesfile
                   ORGANIZATION IS LINE SEQUENTIAL
-                  FILE STATUS  IS FS-CARSALESFILE.
+                  FILE STATUS  IS fs-carsalesfile.
 
        DATA DIVISION.
        FILE SECTION.
-       FD  CARSALESFILE.
-       01  SALESDETAILS.
-           88  ENDOFSALESFILE  VALUE HIGH-VALUES.
-           03  SALESPERSONNAME.
-               05  LASTNAME          PIC X(15).
-               05  FIRSTNAME         PIC X(10).
-           03  QUARTERLYSALES.
-               05  Q1-SALES	     PIC 9(05)V9(02).
-               05  Q2-SALES	     PIC 9(05)V9(02).
-               05  Q3-SALES	     PIC 9(05)V9(02).
-               05  Q4-SALES	     PIC 9(05)V9(02).
-           03  CARTOTAL              PIC 9(07)V9(02).
+       FD  carsalesfile.
+       01  salesdetails.
+           88  endofsalesfile  VALUE HIGH-VALUES.
+           03  salespersonname.
+               05  lastname          PIC X(15).
+               05  firstname         PIC X(10).
+           03  quarterlysales.
+               05  q1-sales	     PIC 9(05)V9(02).
+               05  q2-sales	     PIC 9(05)V9(02).
+               05  q3-sales	     PIC 9(05)V9(02).
+               05  q4-sales	     PIC 9(05)V9(02).
+           03  cartotal              PIC 9(07)V9(02).
 
        WORKING-STORAGE SECTION.
-       77  FS-CARSALESFILE           PIC 9(02)         VALUE ZEROES.
-       77  WS-NAME-CARSALESFILE      PIC X(12)         VALUE SPACES.
+       77  fs-carsalesfile           PIC 9(02)         VALUE ZEROES.
+       77  ws-name-carsalesfile      PIC X(12)         VALUE SPACES.
 
-       78  CTE-01                    VALUE 01.
+       78  cte-01                    VALUE 01.
 
-       01  WS-CAR-SALES.
-           05  WS-AVERAGE-ESTIMATED  PIC 9(12)V9(04)   VALUE ZEROES.
-           05  WS-SALESRECORDS-READ  PIC 9(06)         VALUE ZEROES.
-           05  WS-SALESPERSON-YEARLY PIC 9(08)V9(04)   VALUE ZEROES.
-           05  WS-YEARLY-SALES       PIC 9(12)V9(04)   VALUE ZEROES.
+       01  ws-car-sales.
+           05  ws-average-estimated  PIC 9(12)V9(04)   VALUE ZEROES.
+           05  ws-salesrecords-read  PIC 9(06)         VALUE ZEROES.
+           05  ws-salesperson-yearly PIC 9(08)V9(04)   VALUE ZEROES.
+           05  ws-yearly-sales       PIC 9(12)V9(04)   VALUE ZEROES.
 
-       01  WS-TODAY.
-           05  WS-TODAY-NOW          PIC 9(08)         VALUE ZEROES.
-           05  WS-TODAY-NOW-FMT      PIC 9999/99/99    VALUE ZEROES.
+       01  ws-today.
+           05  ws-today-now          PIC 9(08)         VALUE ZEROES.
+           05  ws-today-now-fmt      PIC 9999/99/99    VALUE ZEROES.
 
-       01  WS-QUARTERLYSALES.
-           05  WS-Q1-SALES           PIC 9(05)V9(02)   VALUE ZEROES.
-           05  WS-Q2-SALES	     PIC 9(05)V9(02)   VALUE ZEROES.
-           05  WS-Q3-SALES	     PIC 9(05)V9(02)   VALUE ZEROES.
-           05  WS-Q4-SALES	     PIC 9(05)V9(02)   VALUE ZEROES.
+       01  ws-quarterlysales.
+           05  ws-q1-sales           PIC 9(05)V9(02)   VALUE ZEROES.
+           05  ws-q2-sales	     PIC 9(05)V9(02)   VALUE ZEROES.
+           05  ws-q3-sales	     PIC 9(05)V9(02)   VALUE ZEROES.
+           05  ws-q4-sales	     PIC 9(05)V9(02)   VALUE ZEROES.
 
-       01  HEADING-LINE.
-           05  FILLER	             PIC X(16) VALUE 'SALESPERSON NAME'.
+       01  heading-line.
+           05  FILLER	             PIC X(16) VALUE 'Salesperson name'.
            05  FILLER	             PIC X(20)         VALUE SPACES.
-           05  FILLER	             PIC X(11) VALUE 'QTR 1 SALES'.
+           05  FILLER	             PIC X(11) VALUE 'Qtr 1 Sales'.
            05  FILLER	             PIC X(02)         VALUE SPACES.
-           05  FILLER	             PIC X(11) VALUE 'QTR 2 SALES'.
+           05  FILLER	             PIC X(11) VALUE 'Qtr 2 Sales'.
            05  FILLER	             PIC X(02)         VALUE SPACES.
-           05  FILLER	             PIC X(11) VALUE 'QTR 3 SALES'.
+           05  FILLER	             PIC X(11) VALUE 'Qtr 3 Sales'.
            05  FILLER	             PIC X(02)         VALUE SPACES.
-           05  FILLER	             PIC X(11) VALUE 'QTR 4 SALES'.
+           05  FILLER	             PIC X(11) VALUE 'Qtr 4 Sales'.
            05  FILLER	             PIC X(03)         VALUE SPACES.
-           05  FILLER 	             PIC X(12) VALUE 'YEARLY SALES'.
+           05  FILLER 	             PIC X(12) VALUE 'Yearly Sales'.
            05  FILLER	             PIC X(02)         VALUE SPACES.
 
-       01  DETAIL-LINE.
+       01  detail-line.
            05  FILLER                PIC X(05)         VALUE SPACES.
-           05  DET-FNAME             PIC X(10)         VALUE SPACES.
+           05  det-fname             PIC X(10)         VALUE SPACES.
            05  FILLER                PIC X(05)         VALUE SPACES.
-           05  DET-LNAME             PIC X(15)         VALUE SPACES.
+           05  det-lname             PIC X(15)         VALUE SPACES.
            05  FILLER                PIC X(02)         VALUE SPACES.
-           05  DET-Q1-SALES          PIC $$$,$$9.99    VALUE ZEROES.
+           05  det-q1-sales          PIC $$$,$$9.99    VALUE ZEROES.
            05  FILLER                PIC X(03)         VALUE SPACES.
-           05  DET-Q2-SALES          PIC $$$,$$9.99    VALUE ZEROES.
+           05  det-q2-sales          PIC $$$,$$9.99    VALUE ZEROES.
            05  FILLER                PIC X(03)         VALUE SPACES.
-           05  DET-Q3-SALES          PIC $$$,$$9.99    VALUE ZEROES.
+           05  det-q3-sales          PIC $$$,$$9.99    VALUE ZEROES.
            05  FILLER                PIC X(03)         VALUE SPACES.
-           05  DET-Q4-SALES          PIC $$$,$$9.99    VALUE ZEROES.
+           05  det-q4-sales          PIC $$$,$$9.99    VALUE ZEROES.
            05  FILLER                PIC X(03)         VALUE SPACES.
-           05  DET-YEARLYSALES       PIC $,$$$,$$9.99  VALUE ZEROES.
+           05  det-yearlysales       PIC $,$$$,$$9.99  VALUE ZEROES.
            05  FILLER                PIC X(19)         VALUE SPACES.
 
-       01  DETAIL-TOTAL-LINE.
+       01  detail-total-line.
            05  FILLER                PIC X(20)         VALUE SPACES.
-           05  FILLER                PIC X(15)   VALUE "TOTALS: ".
+           05  FILLER                PIC X(15)   VALUE "Totals: ".
            05  FILLER                PIC X(02)         VALUE SPACES.
-           05  DET-Q1-TOT-SALES      PIC $$$,$$9.99    VALUE ZEROES.
+           05  det-q1-tot-sales      PIC $$$,$$9.99    VALUE ZEROES.
            05  FILLER                PIC X(03)         VALUE SPACES.
-           05  DET-Q2-TOT-SALES      PIC $$$,$$9.99    VALUE ZEROES.
+           05  det-q2-tot-sales      PIC $$$,$$9.99    VALUE ZEROES.
            05  FILLER                PIC X(03)         VALUE SPACES.
-           05  DET-Q3-TOT-SALES      PIC $$$,$$9.99    VALUE ZEROES.
+           05  det-q3-tot-sales      PIC $$$,$$9.99    VALUE ZEROES.
            05  FILLER                PIC X(03)         VALUE SPACES.
-           05  DET-Q4-TOT-SALES      PIC $$$,$$9.99    VALUE ZEROES.
+           05  det-q4-tot-sales      PIC $$$,$$9.99    VALUE ZEROES.
            05  FILLER                PIC X(02)         VALUE SPACES.
-           05  DET-TOT-YEARLYSALES   PIC $$,$$$,$$9.99 VALUE ZEROES.
+           05  det-tot-yearlysales   PIC $$,$$$,$$9.99 VALUE ZEROES.
            05  FILLER                PIC X(19)         VALUE SPACES.						
 
        PROCEDURE DIVISION.
        DECLARATIVES.
-       FILE-HANDLER SECTION.
-           USE AFTER ERROR PROCEDURE ON CARSALESFILE.
+       File-Handler SECTION.
+           USE AFTER ERROR PROCEDURE ON carsalesfile.
 
-       STATUS-CHECK.
-           DISPLAY "FILE STATUS INFORMATION."
-           DISPLAY "+ NAME OF FILE : [" WS-NAME-CARSALESFILE "]."
-           DISPLAY "+ STATUS CODE  : [" FS-CARSALESFILE "]."
-           STOP "AN EXCEPTION HAS OCCURRED. PRESS ENTER TO CONTINUE...".
+       status-check.
+           DISPLAY "+---+----+---+----+---+----+"
+           DISPLAY "| File Status Information. |"
+           DISPLAY "+---+----+---+----+---+----+"
+           DISPLAY "| + Name of File : [" ws-name-carsalesfile "]."
+           DISPLAY "| + Status Code  : [" fs-carsalesfile "]."
+           DISPLAY "+---+----+---+----+---+----+"
+           STOP "An exception has occurred. Press ENTER to continue...".
        END DECLARATIVES.
 
        MAIN-PARAGRAPH.
-           PERFORM 100-BEGIN-START-PROGRAM
-              THRU 100-END-START-PROGRAM
+           PERFORM 100-begin-start-program
+              THRU 100-end-start-program
 
-           PERFORM 200-BEGIN-PROCESS-SALES-RECORDS
-              THRU 200-END-PROCESS-SALES-RECORDS
-             UNTIL ENDOFSALESFILE
-                OR FS-CARSALESFILE IS NOT EQUAL TO ZEROES
+           PERFORM 200-begin-process-sales-records
+              THRU 200-end-process-sales-records
+             UNTIL endofsalesfile
+                OR fs-carsalesfile IS NOT EQUAL TO ZEROES
 
-           PERFORM 300-BEGIN-FINISH-PROGRAM
-              THRU 300-END-FINISH-PROGRAM
+           PERFORM 300-begin-finish-program
+              THRU 300-end-finish-program
 
            STOP RUN.
 
-       100-BEGIN-START-PROGRAM.
-           DISPLAY "SALES REPORT GENERATOR."
-           DISPLAY "ENTER THE NAME OF THE INPUT FILE: "
+       100-begin-start-program.
+           DISPLAY "Sales Report Generator."
+           DISPLAY "Enter the name of the input file: "
               WITH NO ADVANCING
-            ACCEPT WS-NAME-CARSALESFILE
+            ACCEPT ws-name-carsalesfile
 
-            ACCEPT  WS-TODAY-NOW FROM DATE YYYYMMDD
-              MOVE  WS-TODAY-NOW   TO WS-TODAY-NOW-FMT
+            ACCEPT  ws-today-now FROM DATE YYYYMMDD
+              MOVE  ws-today-now   TO ws-today-now-fmt
 
-           OPEN INPUT CARSALESFILE
-
-           DISPLAY SPACE
-           DISPLAY "TODAY: [" WS-TODAY-NOW-FMT "]."
-           DISPLAY "OPENING. STATUS CODE: [" FS-CARSALESFILE "]."
+           OPEN INPUT carsalesfile
 
            DISPLAY SPACE
-           DISPLAY HEADING-LINE
+           DISPLAY "Today: [" ws-today-now-fmt "]."
+           DISPLAY "Opening. Status Code: [" fs-carsalesfile "]."
+
+           DISPLAY SPACE
+           DISPLAY heading-line
 
            DISPLAY SPACE.
-       100-END-START-PROGRAM.
+       100-end-start-program.
            EXIT.
 
-       200-BEGIN-PROCESS-SALES-RECORDS.
-           READ CARSALESFILE RECORD
+       200-begin-process-sales-records.
+           READ carsalesfile RECORD
              AT END
-                SET ENDOFSALESFILE    TO TRUE
+                SET endofsalesfile    TO TRUE
                 PERFORM 220-BEGIN-SHOW-CONSOLIDATED-TOTAL-LINES
                    THRU 220-END-SHOW-CONSOLIDATED-TOTAL-LINES
 
             NOT AT END
-                PERFORM 210-BEGIN-GENERATE-SALES-DETAIL-LINE
-                   THRU 210-END-GENERATE-SALES-DETAIL-LINE
+                PERFORM 210-BEGIN-GENERATE-SALES-detail-line
+                   THRU 210-END-GENERATE-SALES-detail-line
 
            END-READ.
-       200-END-PROCESS-SALES-RECORDS.
+       200-end-process-sales-records.
            EXIT.
 
-        210-BEGIN-GENERATE-SALES-DETAIL-LINE.
-           ADD  CTE-01                TO WS-SALESRECORDS-READ
+        210-begin-generate-sales-detail-line.
+           ADD  cte-01                TO ws-salesrecords-read
 
-           MOVE FIRSTNAME             TO DET-FNAME
-           MOVE LASTNAME              TO DET-LNAME
-           MOVE Q1-SALES              TO DET-Q1-SALES
-           MOVE Q2-SALES              TO DET-Q2-SALES
-           MOVE Q3-SALES              TO DET-Q3-SALES
-           MOVE Q4-SALES              TO DET-Q4-SALES
+           MOVE firstname             TO det-fname
+           MOVE lastname              TO det-lname
+           MOVE q1-sales              TO det-q1-sales
+           MOVE q2-sales              TO det-q2-sales
+           MOVE q3-sales              TO det-q3-sales
+           MOVE q4-sales              TO det-q4-sales
 
-           ADD  Q1-SALES              TO WS-Q1-SALES
-           ADD  Q2-SALES              TO WS-Q2-SALES 
-           ADD  Q3-SALES              TO WS-Q3-SALES 
-           ADD  Q4-SALES              TO WS-Q4-SALES 
+           ADD  q1-sales              TO ws-q1-sales
+           ADD  q2-sales              TO ws-q2-sales 
+           ADD  q3-sales              TO ws-q3-sales 
+           ADD  q4-sales              TO ws-q4-sales 
 
-           ADD  Q1-SALES  Q2-SALES  Q3-SALES  Q4-SALES
-           GIVING DET-YEARLYSALES
-                  WS-SALESPERSON-YEARLY
+           ADD  q1-sales  q2-sales  q3-sales  q4-sales
+           GIVING det-yearlysales
+                  ws-salesperson-yearly
 
-           ADD  WS-SALESPERSON-YEARLY TO WS-YEARLY-SALES
+           ADD  ws-salesperson-yearly TO ws-yearly-sales
 
-           DISPLAY DETAIL-LINE.
-        210-END-GENERATE-SALES-DETAIL-LINE.
+           DISPLAY detail-line.
+        210-end-generate-sales-detail-line.
            EXIT.
 
-        220-BEGIN-SHOW-CONSOLIDATED-TOTAL-LINES.
-           MOVE WS-SALESPERSON-YEARLY TO CARTOTAL
+        220-begin-show-consolidated-total-lines.
+           MOVE ws-salesperson-yearly TO cartotal
 
-           MOVE WS-Q1-SALES           TO DET-Q1-TOT-SALES
-           MOVE WS-Q2-SALES           TO DET-Q2-TOT-SALES
-           MOVE WS-Q3-SALES           TO DET-Q3-TOT-SALES
-           MOVE WS-Q4-SALES           TO DET-Q4-TOT-SALES
-           MOVE WS-YEARLY-SALES       TO DET-TOT-YEARLYSALES
+           MOVE ws-q1-sales           TO det-q1-tot-sales
+           MOVE ws-q2-sales           TO det-q2-tot-sales
+           MOVE ws-q3-sales           TO det-q3-tot-sales
+           MOVE ws-q4-sales           TO det-q4-tot-sales
+           MOVE ws-yearly-sales       TO det-tot-yearlysales
 
            DISPLAY SPACE
-           DISPLAY DETAIL-TOTAL-LINE.
-        220-END-SHOW-CONSOLIDATED-TOTAL-LINES.
+           DISPLAY detail-total-line.
+        220-end-show-consolidated-total-lines.
            EXIT.
 
-       300-BEGIN-FINISH-PROGRAM.
-           DIVIDE WS-SALESRECORDS-READ      INTO WS-YEARLY-SALES
-           GIVING WS-AVERAGE-ESTIMATED   ROUNDED
+       300-begin-finish-program.
+           DIVIDE ws-salesrecords-read      INTO ws-yearly-sales
+           GIVING ws-average-estimated   ROUNDED
                ON SIZE ERROR
                   DISPLAY SPACE
-                  DISPLAY "THE AVERAGE SALES CANNOT BE DETERMINED."
-                  DISPLAY "THE CALCULATION COULD NOT BE MADE."
+                  DISPLAY "The average sales cannot be determined."
+                  DISPLAY "The calculation could not be made."
            END-DIVIDE
 
            DISPLAY SPACE
-           DISPLAY "SUMMARY"
-           DISPLAY "AVERAGE ESTIMATED SALES : [" WS-AVERAGE-ESTIMATED"]"
-           DISPLAY "RECOVERED SALES RECORDS : [" WS-SALESRECORDS-READ"]"
-           DISPLAY "TOTAL SALES CALCULATED  : [" WS-YEARLY-SALES "]"
+           DISPLAY "Summary"
+           DISPLAY "Average estimated sales : [" ws-average-estimated"]"
+           DISPLAY "Recovered sales records : [" ws-salesrecords-read"]"
+           DISPLAY "Total sales calculated  : [" ws-yearly-sales "]"
 
-           CLOSE CARSALESFILE
+           CLOSE carsalesfile
+
            DISPLAY SPACE
-           DISPLAY "CLOSING. STATUS CODE: [" FS-CARSALESFILE "].".
-       300-END-FINISH-PROGRAM.
+           DISPLAY "Closing. Status Code: [" fs-carsalesfile "].".
+       300-end-finish-program.
            EXIT.
 
        END PROGRAM CarSlsRp.
