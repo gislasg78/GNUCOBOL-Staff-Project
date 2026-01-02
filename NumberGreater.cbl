40dccc3d4c543986cc3378388fd74d72cc96a2e2
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-01-02 16:28:49 -0600

    Mathematical Functions General Project
    The Largest of Three Integers Program.
    This program displays the largest of three numbers and compares it to its two companions.

--- NumberGreater.cbl
+++ NumberGreater.cbl
@@ -4,26 +4,55 @@
        DATA DIVISION.
        WORKING-STORAGE SECTION.
        01  ws-environmental-variables.
-           05  ws-number-1  PIC S9(05)V9(02) COMP-3 VALUE ZEROES.
-           05  ws-number-2  PIC S9(05)V9(02) COMP-3 VALUE ZEROES.
-           05  ws-number-3  PIC S9(05)V9(02) COMP-3 VALUE ZEROES.
+           03  ws-numbers.
+               05  ws-number-1 USAGE FLOAT VALUE ZEROES.
+               05  ws-number-2 USAGE FLOAT VALUE ZEROES.
+               05  ws-number-3 USAGE FLOAT VALUE ZEROES.
+           03  ws-major-number USAGE FLOAT VALUE ZEROES.
 
        PROCEDURE DIVISION.
        MAIN-PARAGRAPH.
-           DISPLAY "Program that validates the largest "
-                   "of three integers.".
+           PERFORM 100000-start-capture-data-numbers
+              THRU 100000-finish-capture-data-numbers
 
-           DISPLAY "Enter the first number : "
+           PERFORM 200000-start-compare-data-numbers
+              THRU 200000-finish-compare-data-numbers
+
+           DISPLAY SPACE
+           DISPLAY "This programa has ended..." WITH NO ADVANCING
+           ACCEPT OMITTED
+
+           STOP RUN.
+
+       100000-start-capture-data-numbers.
+           DISPLAY "Program that validates the largest of three "
+                   "integers.".
+
+           DISPLAY "Enter the first number  : "
               WITH NO ADVANCING
             ACCEPT ws-number-1
 
-           DISPLAY "Enter the second number: "
+           DISPLAY "Enter the second number : "
               WITH NO ADVANCING
             ACCEPT ws-number-2
 
-           DISPLAY "Enter the third number : "
+           DISPLAY "Enter the third number  : "
               WITH NO ADVANCING
-            ACCEPT ws-number-3
+            ACCEPT ws-number-3.
+       100000-finish-capture-data-numbers.
+           EXIT.
+
+       200000-start-compare-data-numbers.
+           PERFORM 210000-start-first-type-of-compare-data
+              THRU 210000-finish-first-type-of-compare-data
+
+           PERFORM 220000-start-second-type-of-compare-data
+              THRU 220000-finish-second-type-of-compare-data.
+       200000-finish-compare-data-numbers.
+           EXIT.
+
+        210000-start-first-type-of-compare-data.
+           DISPLAY SPACE
 
            IF  ws-number-1 >= ws-number-2
            AND ws-number-1 >= ws-number-3
@@ -56,8 +85,35 @@
                                "]."
                    END-IF
                END-IF
-           END-IF
+           END-IF.
+        210000-finish-first-type-of-compare-data.
+           EXIT.
 
-           STOP RUN.
+        220000-start-second-type-of-compare-data.
+           DISPLAY SPACE
+
+           IF ws-number-1 IS GREATER THAN ws-number-2
+              IF ws-number-1 IS GREATER THAN ws-number-3
+                 MOVE ws-number-1 TO ws-major-number
+                 DISPLAY "The first number is the largest: ["
+                          ws-number-1 "]."
+              ELSE
+                 MOVE ws-number-2 TO ws-major-number
+                 DISPLAY "The second number is the largest: ["
+                         ws-number-2 "]."
+              END-IF
+           ELSE
+              IF ws-number-2 IS GREATER THAN ws-number-3
+                 MOVE ws-number-2 TO ws-major-number
+                 DISPLAY "The second number is the largest: ["
+                         ws-number-2 "]."
+              ELSE
+                 MOVE ws-number-3 TO ws-major-number
+                 DISPLAY "The third number is the largest: ["
+                         ws-number-3 "]."
+              END-IF
+           END-IF.
+        220000-finish-second-type-of-compare-data.
+           EXIT.
 
        END PROGRAM NumberGreater.
