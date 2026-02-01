6f7e2155c547502cb06e7de14940873bf759135d
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-02-01 15:27:43 -0600

    Mathematical Functions General Project
    This circle area calculation program calculates the area of ​​a circle given a given radius in GNUCOBOL.

--- CalcCirc.cbl
+++ CalcCirc.cbl
@@ -1,55 +1,120 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. CalcCirc.
+       PROGRAM-ID. CalculateCircle.
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01   ws-constants.
-            03  ws-cte-two                PIC 9(01)        VALUE 2.
-            03  ws-PI                     PIC 9(01)V9(11)
-                                          VALUE 3.14159265359.
-       01   ws-dates.
-            03  ws-date-today             PIC 9(08)        VALUE ZEROES.
+       01   ws-circle-group-outcomes.
+            03  ws-alterante-results.
+                05  ws-circumference      FLOAT-LONG       VALUE ZEROES.
+                05  ws-diameter           FLOAT-LONG       VALUE ZEROES.
+                05  ws-surface            FLOAT-LONG       VALUE ZEROES.
+            03  ws-area-group.
+                05  ws-area-middle        FLOAT-LONG       VALUE ZEROES.
+                05  ws-area-final         FLOAT-LONG       VALUE ZEROES.
+            03  ws-coefficients-group.
+                05  ws-cte-two            FLOAT-SHORT      VALUE 2.
+                05  ws-show-PI            FLOAT-LONG       VALUE ZEROES.
+            03  ws-perimeter-group.
+                05  ws-perimeter-middle   FLOAT-LONG       VALUE ZEROES.
+                05  ws-perimeter-final    FLOAT-LONG       VALUE ZEROES.
+            03  ws-radius-group.
+                05  ws-radius-numeric     FLOAT-LONG       VALUE ZEROES.
+                05  ws-radius-string      PIC X(29)        VALUE SPACES.
+
+       01   ws-dates-group.
+            03  ws-date-today-now         PIC 9(08)        VALUE ZEROES.
             03  ws-date-formatted         PIC 9999/99/99   VALUE ZEROES.
-
-       01   ws-circle.
-            03  ws-area                   PIC 9(07)V9(11)  VALUE ZEROES.
-            03  ws-circumference          PIC 9(07)V9(11)  VALUE ZEROES.
-            03  ws-radius                 PIC 9(07)V9(11)  VALUE ZEROES.
-
-       01   ws-show-values.
-            03  ws-show-area              PIC Z(09)9.9(11) VALUE ZEROES.
-            03  ws-show-circumference     PIC Z(09)9.9(11) VALUE ZEROES.
-            03  ws-show-PI                PIC *(09)9.9(11) VALUE ZEROES.
-            03  ws-show-radius            PIC Z(09)9.9(11) VALUE ZEROES.			 
 
        PROCEDURE DIVISION.
        MAIN-PARAGRAPH.
-           DISPLAY "Calculation of the area and perimeter of a circle."	
-           DISPLAY "Enter radius: " WITH NO ADVANCING
-            ACCEPT  ws-radius
+           PERFORM 100000-start-get-string-radius
+              THRU 100000-finish-get-string-radius
 
-           ACCEPT ws-date-today FROM DATE YYYYMMDD
-             MOVE ws-date-today   TO ws-date-formatted
+           PERFORM 200000-start-obtain-circle-calculations
+              THRU 200000-finish-obtain-circle-calculations
+
+           PERFORM 300000-start-review-circle-calculations-results
+              THRU 300000-finish-review-circle-calculations-results
+
+           PERFORM 400000-start-program-completion
+              THRU 400000-finish-program-completion
+
+           STOP RUN.
+
+       100000-start-get-string-radius.
+           DISPLAY "Calculation of the area and perimeter of a circle."
+           DISPLAY "Enter radius: " WITH NO ADVANCING
+            ACCEPT  ws-radius-string.
+       100000-finish-get-string-radius.
+           EXIT.
+
+       200000-start-obtain-circle-calculations.
+           MOVE FUNCTION NUMVAL(ws-radius-string) TO ws-radius-numeric
+           MOVE FUNCTION PI()                     TO ws-show-PI
+
+           MULTIPLY ws-cte-two                    BY ws-radius-numeric
+             GIVING ws-diameter ROUNDED
+
+           PERFORM 210000-start-find-area-and-perimeter-circle
+              THRU 210000-finish-find-area-and-perimeter-circle
+
+           PERFORM 220000-start-find-circumference-and-surface-circle
+              THRU 220000-finish-find-circumference-and-surface-circle.
+       200000-finish-obtain-circle-calculations.
+           EXIT.
+
+        210000-start-find-area-and-perimeter-circle.
+           MULTIPLY ws-cte-two                    BY FUNCTION PI()
+             GIVING ws-perimeter-middle ROUNDED
+           MULTIPLY ws-perimeter-middle           BY ws-radius-numeric
+             GIVING ws-perimeter-final  ROUNDED
+
+           MULTIPLY ws-radius-numeric             BY ws-radius-numeric
+             GIVING ws-area-middle ROUNDED
+           MULTIPLY FUNCTION PI()                 BY ws-area-middle
+             GIVING ws-area-final  ROUNDED.
+        210000-finish-find-area-and-perimeter-circle.
+           EXIT.
+
+        220000-start-find-circumference-and-surface-circle.
+           COMPUTE ws-circumference ROUNDED = ws-cte-two
+                   * FUNCTION PI() * ws-radius-numeric
+
+           COMPUTE ws-surface ROUNDED = FUNCTION PI()
+                   * (ws-radius-numeric ** ws-cte-two).
+        220000-finish-find-circumference-and-surface-circle.
+           EXIT.
+
+       300000-start-review-circle-calculations-results.
+           ACCEPT ws-date-today-now          FROM DATE YYYYMMDD
+             MOVE ws-date-today-now            TO ws-date-formatted
 
            DISPLAY SPACE
            DISPLAY "[" ws-date-formatted "]."
            DISPLAY "Circle information details."
 
-              MOVE ws-PI          TO ws-show-PI
-           DISPLAY "- PI        : [" ws-show-PI "]."
-
-              MOVE ws-radius      TO ws-show-radius
-           DISPLAY "- Radius    : [" ws-show-radius "]"
+           DISPLAY "+ Coefficient   : [" ws-cte-two "]."
+           DISPLAY "+ PI            : [" ws-show-PI "]."
+           DISPLAY "+ Radius        : [" ws-radius-numeric "]"
+           DISPLAY "+ Diameter      : [" ws-diameter "]."
 
            DISPLAY SPACE
-           COMPUTE ws-circumference, ws-show-circumference =
-                   ws-cte-two * WS-PI * ws-radius
-           DISPLAY "- Perimeter : [" ws-show-circumference "]."
+           DISPLAY "+ Perimeter     : [" ws-perimeter-final "]."
+           DISPLAY "+ Area          : [" ws-area-final "]."
 
-           COMPUTE ws-area, ws-show-area =
-                   ws-PI * (ws-radius ** ws-cte-two)
-           DISPLAY "- Area      : [" ws-show-area "]."
+           DISPLAY SPACE
+           DISPLAY "+ Circumference : [" ws-circumference "]."
+           DISPLAY "+ Surface       : [" ws-surface       "].".
+       300000-finish-review-circle-calculations-results.
+           EXIT.
 
-           STOP RUN.
+       400000-start-program-completion.
+           DISPLAY SPACE
+           DISPLAY "This program has ended."
+           DISPLAY "Press ENTER to end this program..."
+              WITH NO ADVANCING
+            ACCEPT OMITTED.
+       400000-finish-program-completion.
+           EXIT.
 	        
-       END PROGRAM CalcCirc.
+       END PROGRAM CalculateCircle.
