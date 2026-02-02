7bc7de939fb805d754d1799e7859ef2cb5db8fe5
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-02-01 23:01:53 -0600

    Mathematical Functions General Project
    The primary purpose of this program is to exemplify the use of the INSPECT, STRING and UNSTRING statements in GNUCOBOL and thus show their use through the concatenation of elements to form a string with the descriptive date from the current date and then decompose said descriptive date into individual fields with their respective lengths.

--- Strings.cbl
+++ Strings.cbl
@@ -3,63 +3,68 @@
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       78  cte-01                            VALUE 01.
-       78  cte-07                            VALUE 07.
-       78  cte-12                            VALUE 12.
+       78  cte-01                                          VALUE 01.
+       78  cte-07                                          VALUE 07.
+       78  cte-12                                          VALUE 12.
 
        01  ws-accountants-date-and-time-fmt.
            03  ws-accountants-field-string.
-               05  ws-account-field-length   PIC 9(02) VALUE ZEROES.
-               05  ws-account-string-length  PIC 9(02) VALUE ZEROES.
+               05  ws-account-field-length       PIC 9(02) VALUE ZEROES.
+               05  ws-account-string-length      PIC 9(02) VALUE ZEROES.
            03  ws-accountants-spaces.
-               05  ws-account-backspace-len  PIC 9(02) VALUE ZEROES.
-               05  ws-account-frontspace-len PIC 9(02) VALUE ZEROES.
+               05  ws-account-backspace-len      PIC 9(02) VALUE ZEROES.
+               05  ws-account-frontspace-len     PIC 9(02) VALUE ZEROES.
 
        01  ws-current-date-and-time-values.
            03  ws-current-date.
-               05  ws-current-date-year      PIC 9(04) VALUE ZEROES.
-               05  ws-current-date-month     PIC 9(02) VALUE ZEROES.
-               05  ws-current-date-day       PIC 9(02) VALUE ZEROES.
+               05  ws-current-date-year          PIC 9(04) VALUE ZEROES.
+               05  ws-current-date-month         PIC 9(02) VALUE ZEROES.
+               05  ws-current-date-day           PIC 9(02) VALUE ZEROES.
            03  ws-current-date-num REDEFINES ws-current-date PIC 9(08).
            03  ws-current-date-str REDEFINES ws-current-date PIC X(08).
            03  ws-current-time.
-               05  ws-current-time-hour      PIC 9(02) VALUE ZEROES.
-               05  ws-current-time-minute    PIC 9(02) VALUE ZEROES.
-               05  ws-current-time-second    PIC 9(02) VALUE ZEROES.
+               05  ws-current-time-hour          PIC 9(02) VALUE ZEROES.
+               05  ws-current-time-minute        PIC 9(02) VALUE ZEROES.
+               05  ws-current-time-second        PIC 9(02) VALUE ZEROES.
+               05  ws-current-time-hundredths    PIC 9(02) VALUE ZEROES.
 
        01  ws-current-date-and-time-chains-formatted.
-           03  ws-current-date-msg-formatted PIC X(40) VALUE SPACES.
-           03  ws-current-date-num-formatted PIC 9999/99/99
-                                             VALUE ZEROES.
-           03  ws-current-date-str-formatted REDEFINES
-               ws-current-date-num-formatted PIC XXXX/XX/XX.
+           03  ws-current-date-msg-formatted     PIC X(43) VALUE SPACES.
+           03  ws-current-date-num-formatted     PIC 9999/99/99
+                                                 VALUE ZEROES.
+           03  ws-current-date-str-formatted     REDEFINES
+               ws-current-date-num-formatted     PIC XXXX/XX/XX.
            03  ws-current-time-formatted.
-               05  ws-current-time-hour      PIC 9(02) VALUE ZEROES.
-               05  FILLER                    PIC X(01) VALUE X'3A'.
-               05  ws-current-time-minute    PIC 9(02) VALUE ZEROES.
-               05  FILLER                    PIC X(01) VALUE X'3A'.
-               05  ws-current-time-second    PIC 9(02) VALUE ZEROES.
+               05  ws-current-time-hour          PIC 9(02) VALUE ZEROES.
+               05  FILLER                        PIC X(01) VALUE X'3A'.
+               05  ws-current-time-minute        PIC 9(02) VALUE ZEROES.
+               05  FILLER                        PIC X(01) VALUE X'3A'.
+               05  ws-current-time-second        PIC 9(02) VALUE ZEROES.
+               05  FILLER                        PIC X(01) VALUE X'2E'.
+               05  ws-current-time-hundredths    PIC 9(02) VALUE ZEROES.
 
        01  ws-date-and-time-counters-delimiters.
-           03  ws-date-time-counter-fields   PIC 9(02) VALUE ZEROES.
+           03  ws-date-time-counter-fields       PIC 9(02) VALUE ZEROES.
            03  ws-date-counters.
-               05  ws-date-count-dayname     PIC 9(02) VALUE ZEROES.
-               05  ws-date-count-monthname   PIC 9(02) VALUE ZEROES.
-               05  ws-date-count-numday      PIC 9(02) VALUE ZEROES.
-               05  ws-date-count-year        PIC 9(02) VALUE ZEROES.
+               05  ws-date-count-dayname         PIC 9(02) VALUE ZEROES.
+               05  ws-date-count-monthname       PIC 9(02) VALUE ZEROES.
+               05  ws-date-count-numday          PIC 9(02) VALUE ZEROES.
+               05  ws-date-count-year            PIC 9(02) VALUE ZEROES.
            03  ws-date-delimiters.
-               05  ws-date-delimit-dayname   PIC X(02) VALUE SPACES.
-               05  ws-date-delimit-monthname PIC X(02) VALUE SPACES.
-               05  ws-date-delimit-numday    PIC X(02) VALUE SPACES.
-               05  ws-date-delimit-year      PIC X(03) VALUE SPACES.
+               05  ws-date-delimit-dayname       PIC X(02) VALUE SPACES.
+               05  ws-date-delimit-monthname     PIC X(02) VALUE SPACES.
+               05  ws-date-delimit-numday        PIC X(02) VALUE SPACES.
+               05  ws-date-delimit-year          PIC X(03) VALUE SPACES.
            03  ws-time-counters.
-               05  ws-time-count-hour        PIC 9(02) VALUE ZEROES.
-               05  ws-time-count-minute      PIC 9(02) VALUE ZEROES.
-               05  ws-time-count-second      PIC 9(02) VALUE ZEROES.
+               05  ws-time-count-hour            PIC 9(02) VALUE ZEROES.
+               05  ws-time-count-minute          PIC 9(02) VALUE ZEROES.
+               05  ws-time-count-second          PIC 9(02) VALUE ZEROES.
+               05  ws-time-count-hundredths      PIC 9(02) VALUE ZEROES.
            03  ws-time-delimiters.
-               05  ws-time-delimit-hour      PIC X(01) VALUE SPACE.
-               05  ws-time-delimit-minute    PIC X(01) VALUE SPACE.
-               05  ws-time-delimit-second    PIC X(01) VALUE SPACE.
+               05  ws-time-delimit-hour          PIC X(01) VALUE SPACE.
+               05  ws-time-delimit-minute        PIC X(01) VALUE SPACE.
+               05  ws-time-delimit-second        PIC X(01) VALUE SPACE.
+               05  ws-time-delimit-hundredths    PIC X(01) VALUE SPACE.
 
        01  ws-date-and-time-extraction-fields.
            03  ws-current-date-extractions.
@@ -75,32 +80,33 @@
                05  ws-time-hour              PIC 9(02) VALUE ZEROES.
                05  ws-time-minute            PIC 9(02) VALUE ZEROES.
                05  ws-time-second            PIC 9(02) VALUE ZEROES.
+               05  ws-time-hundredths        PIC 9(02) VALUE ZEROES.
 
        01  ws-names-days-and-months-arrays.
            03  ws-date-name-of-days.
-               05  FILLER                PIC A(09) VALUE "Monday".
-               05  FILLER                PIC A(09) VALUE "Tuesday".
-               05  FILLER                PIC A(09) VALUE "Wednesday".
-               05  FILLER                PIC A(09) VALUE "Thursday".
-               05  FILLER                PIC A(09) VALUE "Friday".
-               05  FILLER                PIC A(09) VALUE "Saturday".
-               05  FILLER                PIC A(09) VALUE "Sunday".
+               05  FILLER          PIC A(09) VALUE "Monday".
+               05  FILLER          PIC A(09) VALUE "Tuesday".
+               05  FILLER          PIC A(09) VALUE "Wednesday".
+               05  FILLER          PIC A(09) VALUE "Thursday".
+               05  FILLER          PIC A(09) VALUE "Friday".
+               05  FILLER          PIC A(09) VALUE "Saturday".
+               05  FILLER          PIC A(09) VALUE "Sunday".
            03  ws-date-name-of-days-red REDEFINES ws-date-name-of-days.
                05  ws-date-name-of-days-array      OCCURS cte-07 TIMES
                    INDEXED BY idx-date-name-of-days-array    PIC A(09).
            03  ws-date-name-of-months.
-               05  FILLER                PIC A(09) VALUE "January".
-               05  FILLER                PIC A(09) VALUE "February".
-               05  FILLER                PIC A(09) VALUE "March".
-               05  FILLER                PIC A(09) VALUE "April".
-               05  FILLER                PIC A(09) VALUE "May".
-               05  FILLER                PIC A(09) VALUE "June".
-               05  FILLER                PIC A(09) VALUE "July".
-               05  FILLER                PIC A(09) VALUE "August".
-               05  FILLER                PIC A(09) VALUE "September".
-               05  FILLER                PIC A(09) VALUE "October".
-               05  FILLER                PIC A(09) VALUE "November".
-               05  FILLER                PIC A(09) VALUE "December".
+               05  FILLER          PIC A(09) VALUE "January".
+               05  FILLER          PIC A(09) VALUE "February".
+               05  FILLER          PIC A(09) VALUE "March".
+               05  FILLER          PIC A(09) VALUE "April".
+               05  FILLER          PIC A(09) VALUE "May".
+               05  FILLER          PIC A(09) VALUE "June".
+               05  FILLER          PIC A(09) VALUE "July".
+               05  FILLER          PIC A(09) VALUE "August".
+               05  FILLER          PIC A(09) VALUE "September".
+               05  FILLER          PIC A(09) VALUE "October".
+               05  FILLER          PIC A(09) VALUE "November".
+               05  FILLER          PIC A(09) VALUE "December".
            03  ws-date-name-of-months-red
                REDEFINES ws-date-name-of-months.
                05  ws-date-name-of-months-array    OCCURS cte-12 TIMES
@@ -111,34 +117,51 @@
                05  ws-colon-punctuation-mark PIC X(01) VALUE X'3A'.
                05  ws-comma-punctuation-mark PIC X(01) VALUE X'2C'.
                05  ws-dash-punctuation-mark  PIC X(01) VALUE X'2D'.
+               05  ws-dot-punctuation-mark   PIC X(01) VALUE X'2E'.
            03  ws-punctuations-combinated-marks.
                05  ws-comma-and-normal-space PIC X(02) VALUE X'2C20'.
                05  ws-space-dash-space       PIC X(03) VALUE X'202D20'.
 
        PROCEDURE DIVISION.
        MAIN-PARAGRAPH.
+           PERFORM 100000-begin-get-date-time-system
+              THRU 100000-end-get-date-time-system
+
+           PERFORM 200000-begin-string-current-date
+              THRU 200000-end-string-current-date
+
+           PERFORM 300000-begin-inspect-current-date
+              THRU 300000-end-inspect-current-date
+
+           PERFORM 400000-begin-unstring-current-date
+              THRU 400000-end-unstring-current-date
+
+           PERFORM 500000-begin-display-breakdowns
+              THRU 500000-end-display-breakdowns
+
+           PERFORM 600000-begin-program-completion
+              THRU 600000-end-program-completion
+
+           STOP RUN.
+
+       000000-begin-press-enter-key-to-continue.
+           DISPLAY "Press the ENTER key to continue..."
+              WITH NO ADVANCING
+            ACCEPT OMITTED.
+       000000-end-press-enter-key-to-continue.
+           EXIT.
+
+       100000-begin-get-date-time-system.
            DISPLAY "Program that dismantles a given string of "
                    "characters."
 
            ACCEPT ws-current-date   FROM DATE YYYYMMDD
            ACCEPT ws-date-dayofweek FROM DAY-OF-WEEK
-           ACCEPT ws-current-time   FROM TIME
+           ACCEPT ws-current-time   FROM TIME.
+       100000-end-get-date-time-system.
+           EXIT.
 
-           PERFORM 100000-begin-string-current-date
-              THRU 100000-end-string-current-date
-
-           PERFORM 200000-begin-inspect-current-date
-              THRU 200000-end-inspect-current-date
-
-           PERFORM 300000-begin-unstring-current-date
-              THRU 300000-end-unstring-current-date
-
-           PERFORM 400000-begin-display-breakdowns
-              THRU 400000-end-display-breakdowns
-
-           STOP RUN.
-
-       100000-begin-string-current-date.
+       200000-begin-string-current-date.
            MOVE CORRESPONDING ws-current-time
              TO ws-current-time-formatted
            MOVE SPACES
@@ -174,16 +197,19 @@
                   DELIMITED BY SIZE
                   SPACE
                   DELIMITED BY SIZE
-                  ws-current-time-hour   OF ws-current-time
+                  ws-current-time-hour       OF ws-current-time
                   DELIMITED BY SIZE
                   ws-colon-punctuation-mark
                   DELIMITED BY SIZE
-                  ws-current-time-minute OF ws-current-time
+                  ws-current-time-minute     OF ws-current-time
                   DELIMITED BY SIZE
                   ws-colon-punctuation-mark
                   DELIMITED BY SIZE
-                  ws-current-time-second OF ws-current-time
+                  ws-current-time-second     OF ws-current-time
                   DELIMITED BY SIZE
+                  ws-dot-punctuation-mark
+                  DELIMITED BY SIZE
+                  ws-current-time-hundredths OF ws-current-time
              INTO ws-current-date-msg-formatted
              WITH POINTER ws-date-pointer-string
                ON OVERFLOW
@@ -194,11 +220,14 @@
            END-STRING
 
            DISPLAY SPACE
-           DISPLAY "[" FUNCTION TRIM(ws-current-date-msg-formatted)"].".
-       100000-end-string-current-date.
+           DISPLAY "[" FUNCTION TRIM(ws-current-date-msg-formatted)"]."
+
+           PERFORM 000000-begin-press-enter-key-to-continue
+              THRU 000000-end-press-enter-key-to-continue.
+       200000-end-string-current-date.
            EXIT.
 
-       200000-begin-inspect-current-date.
+       300000-begin-inspect-current-date.
            INITIALIZE ws-account-backspace-len
                       ws-account-frontspace-len
                       ws-account-string-length
@@ -222,11 +251,13 @@
            DISPLAY "Spaces:"
            DISPLAY "+ Back:      [" ws-account-backspace-len "]."
            DISPLAY "+ Front:     [" ws-account-frontspace-len "]."
-           DISPLAY SPACE.
-       200000-end-inspect-current-date.
+
+           PERFORM 000000-begin-press-enter-key-to-continue
+              THRU 000000-end-press-enter-key-to-continue.
+       300000-end-inspect-current-date.
            EXIT.
 
-       300000-begin-unstring-current-date.
+       400000-begin-unstring-current-date.
            MOVE cte-01                       TO ws-date-pointer-unstring
            MOVE SPACES                       TO ws-date-dayname
                                                 ws-date-monthname
@@ -235,10 +266,12 @@
                                                 ws-time-hour
                                                 ws-time-minute
                                                 ws-time-second
+                                                ws-time-hundredths
 
            UNSTRING ws-current-date-msg-formatted
                     DELIMITED BY ws-comma-and-normal-space
                               OR ws-colon-punctuation-mark
+                              OR ws-dot-punctuation-mark
                               OR ws-space-dash-space
                               OR ALL SPACE
                INTO ws-date-dayname
@@ -262,6 +295,9 @@
                     ws-time-second
                         DELIMITER IN ws-time-delimit-second
                         COUNT IN ws-time-count-second
+                    ws-time-hundredths
+                        DELIMITER IN ws-time-delimit-hundredths
+                        COUNT IN ws-time-count-hundredths
                WITH POINTER  ws-date-pointer-unstring
                     TALLYING ws-date-time-counter-fields
                  ON OVERFLOW
@@ -269,11 +305,14 @@
                 NOT ON OVERFLOW
                     DISPLAY "The target fields were successfully "
                             "constructed."
-           END-UNSTRING.
-       300000-end-unstring-current-date.
+           END-UNSTRING
+
+           PERFORM 000000-begin-press-enter-key-to-continue
+              THRU 000000-end-press-enter-key-to-continue.
+       400000-end-unstring-current-date.
            EXIT.
 
-       400000-begin-display-breakdowns.
+       500000-begin-display-breakdowns.
            DISPLAY SPACE
            DISPLAY "Date formats generated."
 
@@ -286,12 +325,18 @@
 
            DISPLAY "[" FUNCTION TRIM(ws-current-date-msg-formatted) "]."
 
+           PERFORM 000000-begin-press-enter-key-to-continue
+              THRU 000000-end-press-enter-key-to-continue
+
            DISPLAY SPACE
            DISPLAY "Extraction Statistics."
            DISPLAY "Pointers."
            DISPLAY "+ String:    [" ws-date-pointer-string "]."
            DISPLAY "+ Unstring:  [" ws-date-pointer-unstring "]."
            DISPLAY "+ Fields:    [" ws-date-time-counter-fields "]."   
+
+           PERFORM 000000-begin-press-enter-key-to-continue
+              THRU 000000-end-press-enter-key-to-continue
 
            DISPLAY SPACE
            DISPLAY "Date."
@@ -315,6 +360,9 @@
            DISPLAY "+ Delimiter: [" ws-date-delimit-year "]."
            DISPLAY "+ Count:     [" ws-date-count-year "]."
 
+           PERFORM 000000-begin-press-enter-key-to-continue
+              THRU 000000-end-press-enter-key-to-continue
+
            DISPLAY SPACE
            DISPLAY "Time."
            DISPLAY "Hour."
@@ -330,8 +378,25 @@
            DISPLAY "Second."
            DISPLAY "+ Value:     [" ws-time-second "]."
            DISPLAY "+ Delimiter: [" ws-time-delimit-second "]."
-           DISPLAY "+ Count:     [" ws-time-count-second "].".
-       400000-end-display-breakdowns.
+           DISPLAY "+ Count:     [" ws-time-count-second "]."
+
+           DISPLAY "Hundredths."
+           DISPLAY "+ Value:     [" ws-time-hundredths "]."
+           DISPLAY "+ Delimiter: [" ws-time-delimit-hundredths "]."
+           DISPLAY "+ Count:     [" ws-time-count-hundredths "]."
+
+           PERFORM 000000-begin-press-enter-key-to-continue
+              THRU 000000-end-press-enter-key-to-continue.
+       500000-end-display-breakdowns.
+           EXIT.
+
+       600000-begin-program-completion.
+           DISPLAY SPACE
+           DISPLAY "This program has ended."
+
+           PERFORM 000000-begin-press-enter-key-to-continue
+              THRU 000000-end-press-enter-key-to-continue.
+       600000-end-program-completion.
            EXIT.
 
        END PROGRAM Strings.
