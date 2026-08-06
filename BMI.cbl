6e8bc508bc126c94b8c54af136dfcea020c76f13
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-08-06 13:20:54 -0600

    Mathematical Functions General Project
    The primary purpose of this program is to calculate the Body Mass Index based on height and weight values, using the formula: BMI = weight / (height * height).

--- BMI.cbl
+++ BMI.cbl
@@ -8,7 +8,7 @@
            03  ws-height-group.
                05  ws-height          PIC 9(03)V9(04) VALUE ZEROES.
                05  ws-height-squared  USAGE FLOAT     VALUE ZERO.
-               03  ws-weight          PIC 9(03)V9(04) VALUE ZEROES.
+           03  ws-weight              PIC 9(03)V9(04) VALUE ZEROES.
 
        PROCEDURE DIVISION.
        MAIN-PARAGRAPH.
@@ -30,12 +30,14 @@
 
           DISPLAY SPACE
           DISPLAY "(BMI) BMI Information."
-          DISPLAY X"2B"     X'20'"BMI:" X'09' X"09" X"7B"
-                  ws-BMI    X"7D" X"2E"
-          DISPLAY X"2D" X'20' "Height:" X'09' X"5B"
-                  ws-height X"5D" X'20' "meters"    X"2E"
-          DISPLAY X"2D" X'20' "Weight:" X'09' X"5B"
-                  ws-weight X"5D" X"20" "kilograms" X"2E"
+          DISPLAY X"2B"  X'20' "BMI:"    X'09' X"09" X"7B"
+                  ws-BMI X"7D" X"2E"
+          DISPLAY X"2D"  X'20' "Height:" X'09' X"5B"
+                  ws-height    X"5D"     X'20' "meters"
+                  X"2E"
+          DISPLAY X"2D"  X'20' "Weight:" X'09' X"5B"
+                  ws-weight    X"5D"     X"20" "kilograms"
+                  X"2E"
           DISPLAY "Press the ENTER key to continue..."
              WITH NO ADVANCING
            ACCEPT OMITTED
