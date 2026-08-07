4aa4b9a47f8f9972cc76fcae0f70f6dd678d1387
Gustavo Islas <136092830+gislasg78@users.noreply.github.com>
2026-08-06 18:42:40 -0600

    Mathematical Functions General Project
    The primary purpose of this program is to calculate the Body Mass Index based on height and weight values, using the formula: BMI = weight / (height * height).

--- BMI.cbl
+++ BMI.cbl
@@ -12,43 +12,77 @@
 
        PROCEDURE DIVISION.
        MAIN-PARAGRAPH.
+           PERFORM 100000-start-request-preliminary-input-data
+              THRU 100000-finish-request-preliminary-input-data
+
+           PERFORM 200000-start-process-BMI-calculations
+              THRU 200000-finish-process-BMI-calculations
+
+           PERFORM 300000-start-show-final-calculations-results
+              THRU 300000-finish-show-final-calculations-results
+
+           STOP RUN.
+
+       100000-start-request-preliminary-input-data.
            DISPLAY "(BMI) Body Mass Index."
            DISPLAY "Enter your height (m): "  WITH NO ADVANCING
             ACCEPT ws-height
            DISPLAY "Enter your weight (kg): " WITH NO ADVANCING
-            ACCEPT ws-weight
+            ACCEPT ws-weight.
+       100000-finish-request-preliminary-input-data.
+           EXIT.
 
-          MULTIPLY ws-height         BY ws-height
-            GIVING ws-height-squared
-          END-MULTIPLY
+        110000-start-request-a-pause-at-the-entrance.
+           DISPLAY "Press the ENTER key to continue..."
+              WITH NO ADVANCING
+            ACCEPT OMITTED.
+        110000-finish-request-a-pause-at-the-entrance.
+           EXIT.
 
-          DIVIDE ws-height-squared INTO ws-weight
-          GIVING ws-BMI
-              ON SIZE ERROR
-                 DISPLAY "Fatal error! Division by zero."
-          END-DIVIDE
+       200000-start-process-BMI-calculations.
+           MULTIPLY ws-height         BY ws-height
+             GIVING ws-height-squared
+           END-MULTIPLY
 
-          DISPLAY SPACE
-          DISPLAY "(BMI) BMI Information."
-          DISPLAY X"2B"  X'20' "BMI:"    X'09' X"09" X"7B"
-                  ws-BMI X"7D" X"2E"
-          DISPLAY X"2D"  X'20' "Height:" X'09' X"5B"
-                  ws-height    X"5D"     X'20' "meters"
-                  X"2E"
-          DISPLAY X"2D"  X'20' "Weight:" X'09' X"5B"
-                  ws-weight    X"5D"     X"20" "kilograms"
-                  X"2E"
-          DISPLAY "Press the ENTER key to continue..."
-             WITH NO ADVANCING
-           ACCEPT OMITTED
+           DISPLAY SPACE
+           DIVIDE ws-height-squared INTO ws-weight
+           GIVING ws-BMI
+               ON SIZE ERROR
+                  DISPLAY "Fatal error!" X'20'
+                          "Division by zero not allowed."
+           NOT ON SIZE ERROR
+                  DISPLAY "Correct!"     X'20'
+                          "Calculations successfully performed."
+           END-DIVIDE.
+       200000-finish-process-BMI-calculations.
+           EXIT.
 
-          DISPLAY SPACE
-          DISPLAY "Done!"
-          DISPLAY "This program has ended."
-          DISPLAY "Press the ENTER key to continue..."
-             WITH NO ADVANCING
-           ACCEPT OMITTED
+       300000-start-show-final-calculations-results.
+           DISPLAY SPACE
+           DISPLAY "(BMI) BMI Information."
+           DISPLAY X"2B" X'20' "BMI"    X"3A" X'09' X"09" X'7B'
+                   ws-BMI      X'7D'    X"2E"
+           DISPLAY X"2D" X'20' "Height" X"3A" X'09' X"5B"
+                   ws-height   X"5D"    X'20' "meters"    X"2E"
+           DISPLAY X"2D" X'20' "Weight" X"3A" X'09' X"5B"
+                   ws-weight   X"5D"    X"20" "kilograms" X"2E"
 
-          STOP RUN.
+           PERFORM 110000-start-request-a-pause-at-the-entrance
+              THRU 110000-finish-request-a-pause-at-the-entrance
+
+           PERFORM 310000-start-program-conclusion-pause
+              THRU 310000-finish-program-conclusion-pause.
+       300000-finish-show-final-calculations-results.
+           EXIT.
+
+        310000-start-program-conclusion-pause.
+           DISPLAY SPACE
+           DISPLAY "Done!"
+           DISPLAY "This program has ended."
+
+           PERFORM 110000-start-request-a-pause-at-the-entrance
+              THRU 110000-finish-request-a-pause-at-the-entrance.
+        310000-finish-program-conclusion-pause.
+           EXIT.
 
        END-PROGRAM. bmi.
